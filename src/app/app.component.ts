b3f408f020d8fcdaf4fde0a40c0e64aedb7e130c
LAPTOP-D3DNISRT\xuant <xuantrang0305@gmail.com>
2024-08-25 16:38:47 +0200

    try to add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   template: `
     <main>
       <header>
         <h2>Pixel Bags</h2>
       </header>
       <section>
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-export const routes: Routes = [];
+import { DetailsComponent } from './details/details.component';
+const routeConfig: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home Page' },
+  { path: 'details', component: DetailsComponent, title: 'Detail Page' },
+];
+export default routeConfig;
