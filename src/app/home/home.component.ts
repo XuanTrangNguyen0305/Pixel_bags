fa202dbeb58540f3b9f2a591629bfcb24b361c60
LAPTOP-D3DNISRT\xuant <xuantrang0305@gmail.com>
2024-08-27 00:16:10 +0200

    added  funtional search bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,24 +13,45 @@ import { BagsService } from '../bags.service';
   template: `
     <section>
       <form>
-        <input placeholder="filter" />
-        <button>Search</button>
+        <input
+          placeholder="filter"
+          #filter
+          (keydown.enter)="filterResults($event, filter.value)"
+        />
+        <button type="button" (click)="filterResults(null, filter.value)">
+          Search
+        </button>
       </form>
     </section>
     <section>
-      <app-bags *ngFor="let bag of bagsList" [bags]="bag"></app-bags>
+      <app-bags *ngFor="let bag of filteredbagsList" [bags]="bag"></app-bags>
     </section>
   `,
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   bagsList: Bags[] = [];
+  filteredbagsList: Bags[] = [];
   bagsService = inject(BagsService); // Inject the BagsService
 
   constructor() {
     // Fetch the bags list from the service
     this.bagsService.getAllBags().then((bagsList: Bags[]) => {
       this.bagsList = bagsList;
+      this.filteredbagsList = bagsList;
     });
+  }
+
+  filterResults(event: Event | null, text: string) {
+    if (event) {
+      event.preventDefault();
+    }
+    if (!text) {
+      this.filteredbagsList = this.bagsList;
+    } else {
+      this.filteredbagsList = this.bagsList.filter((bags) =>
+        bags.name.toLowerCase().includes(text.toLowerCase())
+      );
+    }
   }
 }
